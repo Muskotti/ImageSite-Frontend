c29b654ec95837038d3ec7d9aee48688f14e0990
Toni Vänttinen <Toni.vanttinen@dnainternet.net>
2020-05-17 15:47:01 +0300

    Profile fix Add: - only logged in user can edit loggeds in info

--- src/app/components/profile/settings/settings.component.ts
+++ src/app/components/profile/settings/settings.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GlobalConstants } from '../../../global-constants';
 import { ActivatedRoute } from '@angular/router';
+import Cookies from 'js-cookie';
+import decode from 'jwt-decode';
 
 @Component({
   selector: 'app-settings',
@@ -13,14 +15,20 @@ export class SettingsComponent implements OnInit {
   username: string;
   password: string;
 
+  isLogged = false;
+
   constructor(private http: HttpClient, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const profile = this.router.snapshot.paramMap.get('username');
-    this.http.get<any>(GlobalConstants.apiURL + 'profile/' + profile).subscribe(data => {
-      this.username = data.username;
-      this.password = data.password;
-    });
+    const user = decode(Cookies.get('login'));
+    if (user.username === this.router.snapshot.paramMap.get('username')) {
+      this.isLogged = true;
+      const profile = this.router.snapshot.paramMap.get('username');
+      this.http.get<any>(GlobalConstants.apiURL + 'profile/' + profile).subscribe(data => {
+        this.username = data.username;
+        this.password = data.password;
+      });
+    }
   }
 
   save() {
